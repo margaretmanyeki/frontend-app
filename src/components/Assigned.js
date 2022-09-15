708b0cd9fc410f674cb90d9843b7178b5aa08dc8
margaret manyeki <maggiemanyeki6@gmail.com>
2022-09-15 20:44:25 +0300

    added

--- src/components/Assigned.js
+++ src/components/Assigned.js
@@ -1,13 +1,12 @@
 import React from "react";
 import EmployeesContainer from "./EmployeesContainer";
 
-
 function Assigned({ employees, setEmployees }) {
   return (
     <div className="assigned">
       <EmployeesContainer employees={employees} setEmployees={setEmployees} />
     </div>
   );
-}
+  }
 
 export default Assigned;
